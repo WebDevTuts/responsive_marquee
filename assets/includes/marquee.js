ad9b28a6d51daada69c170cdf5ee5d6b6f5ecb46
edwinchen85 <edwinchen85@gmail.com>
2017-03-29 10:30:32 +0800

    Create a JavaScript object for the variables

---
+++ assets/includes/marquee.js
@@ -0,0 +1,15 @@
+var marqueeVars = {
+
+  screenSize : '',
+  width : 0,
+  mobileSize : 600,
+  autoPlay : true,
+  currentPanel : 1,
+  totalPanels : 0,
+  timePassed : 0,
+  timeToChange : 60,
+  duration : 1250,
+  inTransition : false,
+  panelContent : Array
+
+}
