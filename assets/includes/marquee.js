8d948881776271c1ec1d98d55160ab769637529d
edwinchen85 <edwinchen85@gmail.com>
2017-03-29 10:33:10 +0800

    Populate the HTML debug area with JavaScript variable values

--- assets/includes/marquee.js
+++ assets/includes/marquee.js
@@ -13,3 +13,19 @@ var marqueeVars = {
   panelContent : Array
 
 }
+
+var debugTimer = setInterval(setDebugger, 100)
+
+function setDebugger() {
+
+  $('.var1').html('screenSize = '+marqueeVars.screenSize);
+  $('.var2').html('width = '+marqueeVars.width);
+  $('.var3').html('mobileSize = '+marqueeVars.mobileSize);
+  $('.var4').html('autoPlay = '+marqueeVars.autoPlay);
+  $('.var5').html('currentPanel = '+marqueeVars.currentPanel);
+  $('.var6').html('totalPanels = '+marqueeVars.totalPanels);
+  $('.var7').html('timePassed = '+marqueeVars.timePassed);
+  $('.var8').html('timeToChange = '+marqueeVars.timeToChange);
+  $('.var9').html('inTransition = '+marqueeVars.inTransition);
+
+}
